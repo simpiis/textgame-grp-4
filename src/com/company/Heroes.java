c1d5af0f16ddd56caaf0b7a28700bb26abe0fc2d
André Carrblad0118 <andre.carrblad0118@stud.hkr.se>
2019-12-31 11:52:34 +0100

    Moved magespecial to hero class

--- src/com/company/Heroes.java
+++ src/com/company/Heroes.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Heroes extends Creature {
     private int mageCounter = 0;
     private int rogueMana;
@@ -30,6 +32,82 @@ public class Heroes extends Creature {
 
     public void setRogueMana(int rogueMana) {
         this.rogueMana = rogueMana;
+    }
+    public static int magespecial(int position) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("You can move to: ");
+        if (position >= 2 && position <= 46) {
+            System.out.print( "1. 1 left\n2. 2 left\n3. 1 right \n4. 2 right\n");
+            System.out.print("> ");
+            String choice = input.nextLine();
+            if (choice.equals("1")){
+                position -= 1;
+            } else if (choice.equals("2")) {
+                position -= 2;
+            } else if (choice.equals("3")) {
+                position += 1;
+            } else if (choice.equals("4")) {
+                position += 2;
+            } else {
+                System.out.println("Invalid number");
+            }
+            System.out.println("You moved to room: " + position);
+
+        } else if (position == 0){
+            System.out.print( "1. 1 right \n2. 2 right\n");
+            System.out.print("> ");
+            String choice = input.nextLine();
+            if (choice.equals("1")){
+                position += 1;
+            } else if (choice.equals("2")) {
+                position += 2;
+            } else {
+                System.out.println("Invalid number");
+            }
+            System.out.println("You moved to room: " + position);
+        }else if (position == 1){
+            System.out.print( "1. 1 left\n2. 1 right\n3. 2 right\n");
+            System.out.print("> ");
+            String choice = input.nextLine();
+            if (choice.equals("1")){
+                position -= 1;
+            } else if (choice.equals("2")) {
+                position += 1;
+            } else if (choice.equals("3")) {
+                position += 2;
+            } else {
+                System.out.println("Invalid number");
+            }
+            System.out.println("You moved to room: " + position);
+        } else if (position == 47){
+            System.out.print( "1. 1 right\n2. 1 left \n3. 2 left\n");
+            System.out.print("> ");
+            String choice = input.nextLine();
+            if (choice.equals("1")){
+                position += 1;
+            } else if (choice.equals("2")) {
+                position -= 1;
+            } else if (choice.equals("3")) {
+                position -= 2;
+            } else {
+                System.out.println("Invalid number");
+            }
+            System.out.println("You moved to room: " + position);
+
+        } else if (position == 48){
+            System.out.print( "1. 1 left\n2. 2 left\n");
+            System.out.print("> ");
+            String choice = input.nextLine();
+            if (choice.equals("1")){
+                position -= 1;
+            } else if (choice.equals("2")) {
+                position -= 2;
+            } else {
+                System.out.println("Invalid number");
+            }
+            System.out.println("You moved to room: " + position);
+        }
+        return position;
     }
 }
 
--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -27,7 +27,7 @@ public class Map {
                 System.out.println("1. Yes \n2. No");
                 String choice = input.nextLine();
                 if (choice.equals("1")) {
-                    position = magespecial(position);
+                    position = hero.magespecial(position);
                     Map.printMap(position);
                     int mage = hero.getMageCounter();
                     mage += 1;
@@ -239,83 +239,6 @@ public class Map {
                     direction = "0";
                 }
             }
-        }
-        return position;
-    }
-
-    public static int magespecial(int position) {
-        Scanner input = new Scanner (System.in);
-        System.out.println("You can move to: ");
-        if (position >= 2 && position <= 46) {
-            System.out.print( "1. 1 left\n2. 2 left\n3. 1 right \n4. 2 right\n");
-            System.out.print("> ");
-            String choice = input.nextLine();
-            if (choice.equals("1")){
-                position -= 1;
-            } else if (choice.equals("2")) {
-                position -= 2;
-            } else if (choice.equals("3")) {
-                position += 1;
-            } else if (choice.equals("4")) {
-                position += 2;
-            } else {
-                System.out.println("Invalid number");
-            }
-            System.out.println("You moved to room: " + position);
-
-        } else if (position == 0){
-            System.out.print( "1. 1 right \n2. 2 right\n");
-            System.out.print("> ");
-            String choice = input.nextLine();
-            if (choice.equals("1")){
-                position += 1;
-            } else if (choice.equals("2")) {
-                position += 2;
-            } else {
-                System.out.println("Invalid number");
-            }
-            System.out.println("You moved to room: " + position);
-        }else if (position == 1){
-            System.out.print( "1. 1 left\n2. 1 right\n3. 2 right\n");
-            System.out.print("> ");
-            String choice = input.nextLine();
-            if (choice.equals("1")){
-                position -= 1;
-            } else if (choice.equals("2")) {
-                position += 1;
-            } else if (choice.equals("3")) {
-                position += 2;
-            } else {
-                System.out.println("Invalid number");
-            }
-            System.out.println("You moved to room: " + position);
-        } else if (position == 47){
-            System.out.print( "1. 1 right\n2. 1 left \n3. 2 left\n");
-            System.out.print("> ");
-            String choice = input.nextLine();
-            if (choice.equals("1")){
-                position += 1;
-            } else if (choice.equals("2")) {
-                position -= 1;
-            } else if (choice.equals("3")) {
-                position -= 2;
-            } else {
-                System.out.println("Invalid number");
-            }
-            System.out.println("You moved to room: " + position);
-
-        } else if (position == 48){
-            System.out.print( "1. 1 left\n2. 2 left\n");
-            System.out.print("> ");
-            String choice = input.nextLine();
-            if (choice.equals("1")){
-                position -= 1;
-            } else if (choice.equals("2")) {
-                position -= 2;
-            } else {
-                System.out.println("Invalid number");
-            }
-            System.out.println("You moved to room: " + position);
         }
         return position;
     }
